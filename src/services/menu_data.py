d8de345958d6bdb829ca1bdd6dac13a0282ba254
pedroMontandon <pedrobarbosa89@gmail.com>
2023-09-01 16:40:06 -0300

    MenuData class has been implemented

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,27 @@
+# from multiset import Multiset
+from src.models.dish import Dish
+from src.models.dish import Ingredient
+import csv
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.menu = {}
+        with open(source_path, 'r') as file:
+            c_file = csv.reader(file)
+            next(c_file)
+            for line in file:
+                data = line.split(',')
+                name = data[0]
+                price = float(data[1])
+                ingredient = data[2]
+                amount = int(data[3])
+
+                if name not in self.menu:
+                    self.menu[name] = Dish(name, price)
+                self.menu[name].add_ingredient_dependency(
+                    Ingredient(ingredient), amount
+                )
+                self.dishes.add(self.menu[name])
