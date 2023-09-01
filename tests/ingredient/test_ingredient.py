249afd12c57e1f94ad6eb3cd96200d309a3248ff
pedroMontandon <pedrobarbosa89@gmail.com>
2023-09-01 14:49:07 -0300

    the ingredient tests has been implemented

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,13 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    frango = Ingredient('frango')
+    frango_2 = Ingredient('frango')
+    carne = Ingredient('carne')
+    tropeiro = Ingredient('tropeiro')
+    assert frango.__hash__() == frango_2.__hash__()
+    assert frango.__repr__() == "Ingredient('frango')"
+    assert frango.__eq__(frango_2)
+    assert carne.__hash__() != frango_2.__hash__()
+    assert len(tropeiro.restrictions) == 0
+    assert tropeiro.name == 'tropeiro'
